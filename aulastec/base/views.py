1a636431fe86de387e120f81bd7e161bb8a0896a
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-09-09 22:23:41 -0300

    Alterado mensagem na view base

--- aulastec/base/views.py
+++ aulastec/base/views.py
@@ -7,4 +7,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Aulas TecAcademy!!!!!')
+    return HttpResponse('Plataforma de Aulas TecAcademy, em desenvolvimento!!!!')
