1c3c1bcd709c1195aed38828637b0af30f9bff4c
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-09-17 15:57:12 -0300

    Instalado, configurado e atualizado o template do Bootstrap e layoutit

--- aulastec/base/views.py
+++ aulastec/base/views.py
@@ -1,11 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-
-# from django.shortcuts import render
-
-
-# Create your views here.
 
 def home(request):
-    return HttpResponse('<html><body>Plataforma de Aulas TecAcademy, em desenvolvimento!!!!</body></html>',
-                        content_type='text/html')
+    return render(request, 'base/home.html')
